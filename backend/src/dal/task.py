9ffe367e2999960a214f16700718486b645b5a6b
Emil Muhitdinov <laizybot@gmail.com>
2025-03-20 13:54:30 +0300

    Add task routes

---
+++ backend/src/dal/task.py
@@ -0,0 +1,63 @@
+from datetime import date
+from sqlalchemy.orm import Session
+
+from src.models.task import Task, TaskStatusEnum
+from src.schemas.task import DeadlineFilterEnum, TaskCreate, TaskUpdate
+
+
+def get_task_list(
+    session: Session,
+    user_id: int,
+    page: int = 1,
+    size: int = 10,
+    status: TaskStatusEnum | None = None,
+    deadline: DeadlineFilterEnum | None = None
+) -> tuple[list[Task], int]:
+    query = session.query(Task).where(Task.user_id == user_id)
+
+    if status is not None:
+        query = query.where(Task.status == status)
+    if deadline == DeadlineFilterEnum.ACTUAL:
+        query = query.where(Task.deadline >= date.today())
+    elif deadline == DeadlineFilterEnum.MISSED:
+        query = query.where(Task.deadline < date.today())
+
+    task_list_page = query.offset((page - 1) * size).limit(size).all()
+    task_count = query.count()
+
+    return (task_list_page, task_count)
+
+
+def create_task(session: Session, task: TaskCreate, user_id: int) -> Task:
+    task_db = Task(**task.model_dump(), user_id=user_id)
+    session.add(task_db)
+    session.commit()
+    session.refresh(task_db)
+    return task_db
+
+
+def update_task(
+    session: Session,
+    data: TaskUpdate,
+    user_id: int,
+    task_id: int
+) -> Task:
+    task_db = session.get_one(Task, task_id)
+    if task_db.user_id != user_id:
+        raise ValueError("Task belongs to someone else")
+
+    update_data_dict = data.model_dump(exclude_none=True)
+    for key, value in update_data_dict.items():
+        setattr(task_db, key, value)
+    session.add(task_db)
+    session.commit()
+    session.refresh(task_db)
+    return task_db
+
+
+def delete_task(session: Session, task_id: int, user_id: int) -> None:
+    task_db = session.get_one(Task, task_id)
+    if task_db.user_id != user_id:
+        raise ValueError("Task belongs to someone else")
+    session.delete(task_db)
+    session.commit()
---
+++ backend/src/schemas/task.py
@@ -0,0 +1,50 @@
+from datetime import date
+from enum import Enum
+from pydantic import BaseModel, Field
+
+from src.models.task import TaskStatusEnum
+
+
+class DeadlineFilterEnum(Enum):
+    ACTUAL = "actual"
+    MISSED = "missed"
+
+
+class BaseTask(BaseModel):
+    name: str | None
+    description: str | None
+    status: TaskStatusEnum | None
+    deadline: date | None
+
+
+class TaskCreate(BaseTask):
+    name: str
+    description: str
+    status: TaskStatusEnum
+    deadline: date
+
+
+class TaskPublic(TaskCreate):
+    id: int
+
+
+class TaskUpdate(BaseTask):
+    name: str | None = None
+    description: str | None = None
+    status: TaskStatusEnum | None = None
+    deadline: date | None = None
+
+
+class PaginateParams(BaseModel):
+    page: int = Field(ge=1, default=1)
+    size: int = Field(ge=1, default=1)
+
+
+class FilterParams(PaginateParams):
+    status: TaskStatusEnum | None = None
+    deadline: DeadlineFilterEnum | None = None
+
+
+class TaskPaginated(PaginateParams):
+    total: int = Field(ge=0)
+    data: list[TaskPublic] = []
